54d406e993eea0f441955318f8e5da335b1b88ed
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-05-25 16:27:24 -0600

    feat: Created use background music component

---
+++ shared/hooks/useBackgroundMusic.js
@@ -0,0 +1,50 @@
+import { useEffect, useState, useCallback } from "react";
+
+export const useBackgroundMusic = (musicSource, micSource) => {
+  const ctx = window.AudioContext || window.webkitAudioContext;
+  const [audioContext] = useState(new ctx())
+  const [state, setState] = useState({})
+
+  const getMicrophone = async () => {
+    const microphoneStream = await navigator.mediaDevices.getUserMedia({ audio: { micSource } })
+    const microphone = audioContext.createMediaStreamSource(microphoneStream)
+    return microphone
+  }
+
+  const getBackgroundMusic = () => {
+    const bgMusic = audioContext.createMediaElementSource(musicSource)
+    // bgMusic.connect(audioContext.destination)
+    return bgMusic
+  }
+
+  const mixTracks = (microphone, bgMusic, micGain) => {
+    const mixedOutput = audioContext.createMediaStreamDestination();
+    
+    // Get individaul control of the microphone
+    microphone.connect(micGain)
+    micGain.connect(mixedOutput)
+
+    // Add background music if needed
+    if (bgMusic) { bgMusic.connect(mixedOutput) }
+
+    const mixedAudioTracks = mixedOutput.stream.getAudioTracks()[0]
+    return mixedAudioTracks
+  }
+
+  const init = useCallback(async () => {
+    const microphone = await getMicrophone()
+    const bgMusic = state.bgMusic || getBackgroundMusic()
+    const micGain = audioContext.createGain()
+    const mixedAudioTracks = mixTracks(microphone, bgMusic, micGain)
+
+    setState({ audioContext, microphone, bgMusic, mixedAudioTracks, micGain })
+  }, [musicSource, micSource, state])
+
+  useEffect(() => {
+    if (musicSource && micSource) {
+      init()
+    }
+  }, [musicSource, micSource])
+
+  return state
+}
