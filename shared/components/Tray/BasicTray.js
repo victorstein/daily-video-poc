c2c536cbfd1a8e45ef7dcef6a5da45ca356b7e5f
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-05-25 16:26:59 -0600

    feat: implemented music player component and bind data streams appropriately

--- shared/components/Tray/BasicTray.js
+++ shared/components/Tray/BasicTray.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { NETWORK_ASIDE } from '@custom/shared/components/Aside/NetworkAside';
 import { PEOPLE_ASIDE } from '@custom/shared/components/Aside/PeopleAside';
 import Button from '@custom/shared/components/Button';
@@ -17,13 +17,22 @@ import { ReactComponent as IconNetwork } from '../../icons/network-md.svg';
 import { ReactComponent as IconPeople } from '../../icons/people-md.svg';
 import { ReactComponent as IconSettings } from '../../icons/settings-md.svg';
 import { Tray, TrayButton } from './Tray';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { useParticipants } from '@custom/shared/contexts/ParticipantsProvider';
+import { useBackgroundMusic } from '@custom/shared/hooks/useBackgroundMusic';
 
 export const BasicTray = () => {
+  const bgMusicComponent = useRef(null);
   const responsive = useResponsive();
   const [showMore, setShowMore] = useState(false);
   const { callObject, leave } = useCallState();
   const { customTrayComponent, openModal, toggleAside } = useUIState();
-  const { isCamMuted, isMicMuted } = useMediaDevices();
+  const { isCamMuted, currentDevices: { mic } } = useMediaDevices();
+  const [isMicMuted, setIsMicMuted] = useState(false)
+  const { mixedAudioTracks, micGain } = useBackgroundMusic(bgMusicComponent?.current?.audio?.current, mic)
+
+  const { isOwner } = useParticipants();
 
   const toggleCamera = (newState) => {
     if (!callObject) return false;
@@ -31,91 +40,120 @@ export const BasicTray = () => {
   };
 
   const toggleMic = (newState) => {
+    setIsMicMuted(!newState)
+    if (micGain) {
+      return micGain.gain.value = Number(newState)
+    }
+    
     if (!callObject) return false;
     return callObject.setLocalAudio(newState);
   };
 
+  useEffect(() => {
+    const mixAudioTracks = async () => {
+      await callObject.setInputDevicesAsync({
+        audioSource: mixedAudioTracks,
+      });
+    }
+
+    if (mixedAudioTracks) {
+      mixAudioTracks()
+    }
+  }, [mixedAudioTracks])
+
   return (
-    <Tray className="tray">
-      <TrayButton
-        label="Camera"
-        onClick={() => toggleCamera(isCamMuted)}
-        orange={isCamMuted}
-      >
-        {isCamMuted ? <IconCameraOff /> : <IconCameraOn />}
-      </TrayButton>
-      <TrayButton
-        label="Mic"
-        onClick={() => toggleMic(isMicMuted)}
-        orange={isMicMuted}
-      >
-        {isMicMuted ? <IconMicOff /> : <IconMicOn />}
-      </TrayButton>
-      {responsive.isMobile() && showMore && (
-        <div className="more-options">
-          <Button
-            className="translucent"
-            onClick={() => openModal(DEVICE_MODAL)}
-            IconBefore={IconSettings}
-          >
-            Settings
-          </Button>
-          <Button
-            className="translucent"
-            onClick={() => toggleAside(NETWORK_ASIDE)}
-            IconBefore={IconNetwork}
-          >
-            Network
-          </Button>
-          <Button
-            className="translucent"
-            onClick={() => toggleAside(PEOPLE_ASIDE)}
-            IconBefore={IconPeople}
-          >
-            People
-          </Button>
-        </div>
-      )}
-      {!responsive.isMobile() ? (
-        <>
-          <TrayButton label="Settings" onClick={() => openModal(DEVICE_MODAL)}>
-            <IconSettings />
-          </TrayButton>
-          <TrayButton label="Network" onClick={() => toggleAside(NETWORK_ASIDE)}>
-            <IconNetwork />
-          </TrayButton>
-          <TrayButton label="People" onClick={() => toggleAside(PEOPLE_ASIDE)}>
-            <IconPeople />
-          </TrayButton>
-        </>
-      ) : (
-        <TrayButton label="More" onClick={() => setShowMore(!showMore)}>
-          <IconMore />
+    <>
+      {
+        isOwner &&
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <AudioPlayer
+              ref={bgMusicComponent}
+              src="assets/bg_music.mp3"
+              style={{ maxWidth: '70%' }}
+            />
+          </div>
+      }
+      <Tray className="tray">
+        <TrayButton
+          label="Camera"
+          onClick={() => toggleCamera(isCamMuted)}
+          orange={isCamMuted}
+        >
+          {isCamMuted ? <IconCameraOff /> : <IconCameraOn />}
         </TrayButton>
-      )}
+        <TrayButton
+          label="Mic"
+          onClick={() => toggleMic(isMicMuted)}
+          orange={isMicMuted}
+        >
+          {isMicMuted ? <IconMicOff /> : <IconMicOn />}
+        </TrayButton>
+        {responsive.isMobile() && showMore && (
+          <div className="more-options">
+            <Button
+              className="translucent"
+              onClick={() => openModal(DEVICE_MODAL)}
+              IconBefore={IconSettings}
+            >
+              Settings
+            </Button>
+            <Button
+              className="translucent"
+              onClick={() => toggleAside(NETWORK_ASIDE)}
+              IconBefore={IconNetwork}
+            >
+              Network
+            </Button>
+            <Button
+              className="translucent"
+              onClick={() => toggleAside(PEOPLE_ASIDE)}
+              IconBefore={IconPeople}
+            >
+              People
+            </Button>
+          </div>
+        )}
+        {!responsive.isMobile() ? (
+          <>
+            <TrayButton label="Settings" onClick={() => openModal(DEVICE_MODAL)}>
+              <IconSettings />
+            </TrayButton>
+            <TrayButton label="Network" onClick={() => toggleAside(NETWORK_ASIDE)}>
+              <IconNetwork />
+            </TrayButton>
+            <TrayButton label="People" onClick={() => toggleAside(PEOPLE_ASIDE)}>
+              <IconPeople />
+            </TrayButton>
+          </>
+        ) : (
+          <TrayButton label="More" onClick={() => setShowMore(!showMore)}>
+            <IconMore />
+          </TrayButton>
+        )}
 
-      {customTrayComponent}
+        {customTrayComponent}
 
-      <span className="divider" />
+        <span className="divider" />
 
-      <TrayButton label="Leave" onClick={() => leave()} orange>
-        <IconLeave />
-      </TrayButton>
-      <style jsx>{`
-        .tray { position: relative };
-        .more-options {
-          background: var(--background);
-          position: absolute;
-          transform: translateX(calc(-50% + 26px));
-          bottom: calc(15% + var(--spacing-xxxs));
-          z-index: 99;
-          padding: var(--spacing-xxxs);
-          border-radius: var(--radius-md);
-          box-shadow: var(--shadow-depth-2);
-        }
-      `}
-      </style>
-    </Tray>
+        <TrayButton label="Leave" onClick={() => leave()} orange>
+          <IconLeave />
+        </TrayButton>
+        <style jsx>{`
+          .tray { position: relative };
+          .more-options {
+            background: var(--background);
+            position: absolute;
+            transform: translateX(calc(-50% + 26px));
+            bottom: calc(15% + var(--spacing-xxxs));
+            z-index: 99;
+            padding: var(--spacing-xxxs);
+            border-radius: var(--radius-md);
+            box-shadow: var(--shadow-depth-2);
+          }
+        `}
+        </style>
+      </Tray>
+    </>
   );
 };
 export default BasicTray;
